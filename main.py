a78f739e61d62252a04f0060f532bf169bc546f4
David Zhu <63690620+davidzhuuwa@users.noreply.github.com>
2021-07-26 10:09:32 +0000

    Expanded word list and separated wordlist and art into separate modules. Also added quality of life checks that inform user if they have already guessed a letter, and informing the user if a letter they guessed is incorrect.

--- main.py
+++ main.py
@@ -1,82 +1,39 @@
 # Importing modules
 import random as rd
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-word_list = ["aardvark", "baboon", "camel"]
+from hangman_art import stages, logo
+from hangman_words import word_list
+from replit import clear
 
 chosen_word = rd.choice(word_list)
 word_length = len(chosen_word)
 
+print(logo)
 # setting number of lives 
 lives = 6
 
 # Testing code
-print(f'Pssst, the solution is {chosen_word}.')
-
+# print(f'Pssst, the solution is {chosen_word}.')
 
 # Creating blanks
 display = []
+guess_list = []
 for _ in range(word_length):
     display+= "_"
 word_complete = False
 while not word_complete:
     guess = input('Guess a letter : ').lower()
     inword = False
+
+    # using clear() to clear output between guesses
+    clear() 
+    
+    # Checking that user hasn't already guessed the letter
+    if guess not in guess_list:
+        guess_list.append(guess)
+    else:
+        print(f'The letter {guess} has already been guessed.')
+        continue 
+
     # Checking guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
@@ -84,20 +41,20 @@ while not word_complete:
         if letter == guess:
             display[position] = letter 
             inword = True
+
     # Reducing number of lives if guess was not in word 
     if not inword:
+        print(f'The letter {guess} was not in the word')
         lives-=1
 
-    # Join all the elemements in the list and turn it int a String 
+    # Join all the elements in the list and turn it int a String 
     print(f"{' '.join(display)}")
     print(stages[lives])
-    print(lives)
     if not any(letter == '_' for letter in display):
         word_complete = True
     if (lives == 0): 
         break
-        
 if word_complete:
-    print(f'The final word is {chosen_word}!')
+    print(f'The final word is {chosen_word}! You win!')
 else:
     print('Game Over: you ran out of lives!')
