71a53bc2784708526ac2a12b329c7ff4d546f93a
David Zhu <63690620+davidzhuuwa@users.noreply.github.com>
2021-07-26 09:43:32 +0000

    Added while loop that iterates until the word is complete

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ import random as rd
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = rd.choice(word_list)
-
+word_length = len(chosen_word)
 
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-# Create an empty List called display
-# For each letter in the chosen word, add a "_" to 'display'
 
+# Creating blanks
 display = []
-for i in range(0,len(chosen_word)):
-    display.append('_')
-print(display)
-
-guess = input('Guess a letter : ').lower()
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-i = 0
-for letter in chosen_word:
-    if guess == letter:
-        display[i] = letter
-    else:
-        print('Wrong')
-    i+=1
-print(display)
+for _ in range(word_length):
+    display+= "_"
+word_complete = False
+while not word_complete:
+    guess = input('Guess a letter : ').lower()
+    
+    # Checking guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f'Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}')
+        if letter == guess:
+            display[position] = letter 
+    print(display)
+    if not any(letter == '_' for letter in display):
+        word_complete = True
+print(f'The final word is {chosen_word}!')
