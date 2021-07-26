c80c124cd86d0c3d8b74937d20d65a0b4625d460
David Zhu <63690620+davidzhuuwa@users.noreply.github.com>
2021-07-26 09:23:52 +0000

    Implemented check for correct letters in chosen word

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+# Importing modules
+import random as rd
+
 #Step 1 
 
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = rd.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input('Guess a letter : ').lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for letter in chosen_word:
+    if guess == letter:
+        print(f'Letter {guess} is in the word')
+    else:
+        print('Wrong')
